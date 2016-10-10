ff5e82202c3d2c563fb95333076c98a7249693ea
llancon <rdzalb@gmail.com>
2016-10-10 17:40:32 -0400

    Added coffee class and cleared error message all test have passed green

--- coffee.rb
+++ coffee.rb
@@ -1,3 +1,21 @@
 class Coffee
-  
+  attr_accessor :number_of_drinks, :name
+    def initialize(name)
+      self.number_of_drinks = 3
+      self.name = name
+end
+  def full?
+  number_of_drinks == 3
+
+  end
+  def drink!
+    if number_of_drinks.to_i < 1
+      coffee="full"
+    end
+
+  end
+  def empty?
+    number_of_drinks == 0
+  end
+
 end
--- human.rb
+++ human.rb
@@ -1,11 +1,32 @@
 class Human
   attr_accessor :name,
-                :alertness
+                :alertness,
+                :coffee,
+                :drink
 
   def initialize(name, alertness=0)
     self.name = name
     self.alertness = alertness
+    self.coffee = 0
   end
-
-
+  def has_coffee?
+    coffee.to_i > 0
+  end
+  def needs_coffee?
+    self.coffee.to_i == 0
+  end
+  def full?
+    coffee.to_i > 0
+    self.coffee = 1
+  end
+  def buy(coffee)
+    self.coffee = coffee
+  end
+  def drink!
+    self.coffee.number_of_drinks -= 1
+    self.alertness +=0.31
+  end
+  def empty?
+    self.coffee < 0.1
+  end
 end
